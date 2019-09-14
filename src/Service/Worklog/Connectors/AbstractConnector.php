3fb3bd933dde40947e3dd53a6e586d558348a843
Cristian Radulescu <cristian.radulescu@emag.ro>
2019-09-14 03:30:17 +0300

    Update duration display

--- src/Service/Worklog/Connectors/AbstractConnector.php
+++ src/Service/Worklog/Connectors/AbstractConnector.php
@@ -4,6 +4,7 @@ namespace App\Service\Worklog\Connectors;
 
 use App\Service\Worklog\DataSource\AbstractDataSource;
 use App\Service\Worklog\DataSource\FileDataSource;
+use App\Service\Worklog\Formatter\Duration;
 use App\Service\Worklog\WorklogDto;
 use App\Service\Worklog\WorklogItemDto;
 use Carbon\CarbonInterval;
@@ -31,7 +32,7 @@ abstract class AbstractConnector
             $worklogItem->name = $item['name'] ?? null;
             $worklogItem->duration = $item['duration'] ?? null;
             if (null !== $worklogItem->duration) {
-                $worklogItem->durationFormatted = $this->formatWorklogItemDuration($worklogItem);
+                $worklogItem->durationFormatted = Duration::format($worklogItem->duration);
             }
             $worklogItem->content = $item['content'] ?? $this->getDefaultContent($worklogItem->name);
             $worklogItem->taskType = $item['task_type'] ?? $this->getStandardTaskType();
@@ -40,20 +41,6 @@ abstract class AbstractConnector
         }
 
         return $worklog;
-    }
-
-    private function formatWorklogItemDuration(WorklogItemDto $worklogItem): string
-    {
-        $interval = CarbonInterval::seconds($worklogItem->duration);
-        $formattedDate = $interval->totalDays >= 1 ? $interval->totalDays.'d' : '';
-        $formattedDate .= $interval->totalHours >= 1 ? ' '.$interval->totalHours.'h' : '';
-        if (1 >= $interval->totalHours && ($interval->totalHours * 60 - $interval->totalMinutes) >= 1) {
-            $formattedDate .= ' '.$interval->totalMinutes.'m';
-        } elseif (1 > $interval->totalHours) {
-            $formattedDate .= ' '.$interval->totalMinutes.'m';
-        }
-
-        return $formattedDate;
     }
 
     public function getStandardTaskType()
